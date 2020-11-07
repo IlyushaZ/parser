11daf996d1692c374158efce6302138dc0c18acd
IlyushaZ <ilyusha.god@yandex.ru>
2020-11-07 12:38:52 +0300

    added docker-compose up to travis-ci and increased time of waiting for db by app

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-echo "Checking DB connection ..."
+echo "Checking DB connection..."
 
 i=0
 until [ $i -ge 10 ]
@@ -11,16 +11,16 @@ do
 
   i=$(( i + 1 ))
 
-  echo "$i: Waiting for DB 1 second ..."
-  sleep 1
+  echo "$i: Waiting for DB 3 second..."
+  sleep 3
 done
 
 if [ $i -eq 10 ]
 then
-  echo "DB connection refused, terminating ..."
+  echo "DB connection refused, terminating..."
   exit 1
 fi
 
-echo "DB is up ..."
+echo "DB is up..."
 
 /bin/parser
